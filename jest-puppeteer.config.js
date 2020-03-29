b27fed22945db1205b3f217f1871bf4f83a80611
Ivan Kasabov <ivan@midiaresearch.com>
2020-03-29 20:22:13 +0100

    Removed puppeteer e2e tests

--- jest-puppeteer.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    launch: {},
-    server: {
-        command: 'npm start',
-        port: 8008,
-        launchTimeout: 60000
-    }
-};
--- src/ts/tests/todo-handler.test.ts
+++ src/ts/tests/todo-handler.test.ts
@@ -1,7 +1,6 @@
 import { addTodo, editTodo, deleteTodo } from '../util/todo-handler'
 import { generateRandomId } from '../util/id-randomiser'
 import { Todo } from '../constants/types';
-import puppeteer from 'puppeteer';
 test('should add a new todo', () => {
     const emptyTodos: Todo[] = [];
     const initialTodo: Todo = {
@@ -96,75 +95,3 @@ test('should delete a todo', () => {
         .toEqual([initialTodos[0], initialTodos[2]]);
 
 });
-
-test('testing form validation and todo CRUD operations', async () => {
-    const browser = await puppeteer.launch()
-
-    const page = await browser.newPage();
-    await page.goto('http://localhost:8008/');
-
-    // try to submit an empty form
-    await page.click('#addTodo');
-    const nameError = await page.$eval('#name-error', el => el.textContent)
-    expect(nameError).toStrictEqual('Required field');
-    const descriptionError = await page.$eval('#description-error', el => el.textContent)
-    expect(descriptionError).toStrictEqual('Required field');
-
-    // add a todo
-    await page.click('#name');
-    await page.type('#name', 'Programmed todo');
-    await page.click('#description');
-    await page.type('#description', 'Programmed description');
-    await page.click('#addTodo');
-
-    const todoName = await page.$eval('.todo-item__name', el => el.textContent)
-    expect(todoName).toStrictEqual('Programmed todo');
-
-    // try adding a todo without a description or name
-    await page.click('#name');
-    await page.type('#name', 'This one has no description');
-    await page.click('#addTodo');
-
-    const validationError = await page.$eval('#description-error', el => el.textContent)
-    expect(validationError).toStrictEqual('Required field');
-
-    await page.click('#description');
-    await page.type('#description', 'It does now, but let\'s delete the name');
-    await page.click('#name');
-    await page.evaluate(() => document.execCommand('selectall', false));
-    await page.keyboard.press('Backspace');
-    await page.click('#name');
-    await page.type('#name', 'Re-added the name');
-    await page.click('#addTodo');
-
-    // edit name and description
-    const todoItem = await page.$('.todo-item');
-    await page.click('.todo-item__controls-edit');
-    await page.evaluate(() => document.execCommand('selectall', false));
-    await page.keyboard.press('Backspace');
-    await todoItem!.type('Edited name');
-    await page.keyboard.press('Tab');
-    await todoItem!.type('Edited description');
-    await todoItem!.press('Enter');
-
-    const newName = await page.$eval('.todo-item__name', el => el.textContent)
-    expect(newName).toStrictEqual('Edited name');
-
-    const description = await page.$eval('.todo-item__description', el => el.textContent)
-    expect(description).toStrictEqual('Edited description');
-
-    // delete a todo
-    const todoList = await page.$$('.todo-item');
-    await page.click('.todo-item__controls-delete');
-    const reducedTodoList = await page.$$('.todo-item');
-    expect(reducedTodoList.length).toStrictEqual(todoList.length - 1);
-
-    // delete the last todo
-    await page.click('.todo-item__controls-delete');
-
-    const emptyListMessage = await page.$eval('.todo-list__empty', el => el.textContent)
-    expect(emptyListMessage).toStrictEqual('No todos found');
-
-    browser.close();
-
-}, 10000)
