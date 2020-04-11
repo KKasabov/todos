4f080183b5cd66b501d4a92c1352983d401d7149
Ivan Kasabov <ivan@midiaresearch.com>
2020-04-11 14:17:09 +0100

    Date formatter test in a separate jest describe

--- src/ts/tests/date-formatter.test.ts
+++ src/ts/tests/date-formatter.test.ts
@@ -1,7 +1,9 @@
 import { formatDate } from '../util/date-formatter';
 import 'jest-extended';
 
-test('should format a date in DD MMM YYYY format', () => {
-    const date = new Date('2020-02-02');
-    expect(formatDate(date)).toStrictEqual('2 Feb 2020 00:00:00');
+describe('testing date formatter', () => {
+    it('should format a date in DD MMM YYYY format', () => {
+        const date = new Date('2020-02-02');
+        expect(formatDate(date)).toStrictEqual('2 Feb 2020 00:00:00');
+    })
 })
