881634ff41aad6be9ecafd571e432d138a6b199a
Ivan Kasabov <ivan@midiaresearch.com>
2020-04-24 23:31:12 +0100

    Todos and recordings stored in localStorage

--- src/ts/store/index.ts
+++ src/ts/store/index.ts
@@ -1,8 +1,16 @@
 import { createStore, applyMiddleware, Middleware } from 'redux';
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import reducer, { initialState } from './reducer';
 import { AppActionType, RECORD_ACTION } from './types';
+
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
 
 // middleware for recording redux actions
 const recordAction: Middleware = store => next => (action: AppActionType) => {
@@ -17,10 +25,9 @@ const recordAction: Middleware = store => next => (action: AppActionType) => {
     next(action);
 }
 
-const store = createStore(
-    reducer,
-    initialState,
+export const store = createStore(
+    persistReducer(persistConfig, reducer),
     composeWithDevTools(applyMiddleware(thunkMiddleware, recordAction))
 );
 
-export default store;
+export const persistor = persistStore(store);
