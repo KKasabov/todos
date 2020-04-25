5a5414b0cf30e04fdf50725758121d9f5633f2ca
Ivan Kasabov <ivan@midiaresearch.com>
2020-04-25 02:42:19 +0100

    Corrected folders

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/dist')));
 
     // Handle React routing, return all requests to React app
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
     });
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
