56c3638aee79e522389feb90091a58bf6533b01d
Ivan Kasabov <ivan@midiaresearch.com>
2020-04-25 18:30:02 +0100

    updated server.js

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + '/client/dist'));
+app.use(express.static(__dirname + '/dist'));
 
-// send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
